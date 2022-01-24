8b15bd37d4069d1655531da7195ccde318ff6a70
dw1a <dw1a@DW1A-07>
2022-01-24 09:35:43 +0100

    Cambios en Principal

--- proyecto1/src/prueba1/Principal.java
+++ proyecto1/src/prueba1/Principal.java
@@ -4,5 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!");
 		System.out.println("Segundo cambio");
+		System.out.println("Clase Principal");
+		System.out.println("Pruebas git, clase Principal.");
 	}
 }
